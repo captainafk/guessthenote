811acc4b00e5f6f092c473e19906a26b71b2ad62
captainafk <cem.kobal@gmail.com>
2022-05-15 01:45:21 +0300

    Scrolling background added

---
+++ GTN/Assets/Scripts/MainMenu/ScrollingBackground.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GuessTheNote
+{
+    public class ScrollingBackground : MonoBehaviour
+    {
+        [SerializeField] float _speedX = 0.1f;
+        [SerializeField] float _speedY = 0.1f;
+
+        private RawImage _image;
+
+        private void Awake()
+        {
+            _image = GetComponent<RawImage>();
+        }
+
+        private void Update()
+        {
+            _image.uvRect = new Rect(_image.uvRect.position +
+                                        new Vector2(_speedX, _speedY) * Time.deltaTime,
+                                     _image.uvRect.size);
+        }
+    }
+}
